a2bfb91534b4edb8cc9b093a1c58e31465ca995e
Sajib <sajib202040@gmail.com>
2026-01-19 00:16:20 +0600

    Assignment 2: Database Design and Migrations completed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Primary Key (BIGINT)
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile');
             $table->string('password');
+            $table->string('otp');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
